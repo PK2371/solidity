322b90f997c317df44dda94100c15f7b1b20129e
Priyanshu Kumar <103568343+PK2371@users.noreply.github.com>
2022-08-05 22:46:43 +0530

    Add files via upload

---
+++ SimpleStorage.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+    uint256 transactionid;
+
+    // This is a comment!
+    struct User {
+        uint256 transactionid;
+        string name;
+    }
+
+    User[] public user;
+    mapping(string => uint256) public nameToTransactionid;
+
+    function store(uint256 _transactionid) public {
+        transactionid = _transactionid;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return transactionid;
+    }
+
+    function addUser(string memory _name, uint256 _transactionid) public {
+        user.push(User(_transactionid, _name));
+        nameToTransactionid[_name] = _transactionid;
+    }
+}
